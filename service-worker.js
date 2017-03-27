2c58bb78675e9191a4c24ee65889a8581977ff69
christopherdale <christopher.j.dale@baesystems.com>
2017-03-27 15:32:18 +0100

    add fetch eventListener

--- service-worker.js
+++ service-worker.js
@@ -33,3 +33,17 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('Finally active. Ready to start serving content!');  
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
