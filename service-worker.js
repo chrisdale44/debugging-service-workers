f1efcf689e1953a0b21235c2510ee956b0ffa8c5
christopherdale <christopher.j.dale@baesystems.com>
2017-03-27 15:45:52 +0100

    add push notification

--- service-worker.js
+++ service-worker.js
@@ -47,3 +47,19 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+// Push notification handler. Not for production.
+// See this post: https://developers.google.com/web/updates/2015/03/push-notifications-on-the-open-web?hl=en
+self.addEventListener('push', function(event) {  
+  var title = 'Yay a message.';  
+  var body = 'We have received a push message.';  
+  var icon = '/images/smiley.svg';  
+  var tag = 'simple-push-example-tag';
+  event.waitUntil(  
+    self.registration.showNotification(title, {  
+      body: body,  
+      icon: icon,  
+      tag: tag  
+    })  
+  );  
+});
